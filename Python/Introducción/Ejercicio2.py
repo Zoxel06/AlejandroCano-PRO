0cf25cc2f2d3020aebd323ee1e30402f1c0cda45
Zoxel06 <zoxel14@gmail.com>
2025-09-15 23:10:29 +0200

    Update

---
+++ Python/Introducción/Ejercicio2.py
@@ -0,0 +1,3 @@
+lado = float(input("Ingresa la longitud del lado del cuadrado: "))
+area = lado * lado
+print("El área es:", area)
---
+++ Python/Introducción/Ejercicio5.py
@@ -0,0 +1,4 @@
+cadena1 = input("Ingresa la primera cadena: ")
+cadena2 = input("Ingresa la segunda cadena: ")
+cadenaFinal = cadena1 + " " + cadena2
+print(cadenaFinal)
