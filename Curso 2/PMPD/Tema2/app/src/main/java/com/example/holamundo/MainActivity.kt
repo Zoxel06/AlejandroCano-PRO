af97c7bf572b19bbfc1bc8e1a7d2f024fe484f26
Zoxel06 <zoxel14@gmail.com>
2025-10-16 14:29:47 +0200

    Update

--- Curso 2/PMPD/Tema2/app/src/main/java/com/example/holamundo/MainActivity.kt
+++ Curso 2/PMPD/Tema2/app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -2,16 +2,41 @@ package com.example.holamundo
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.holamundo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button:Button
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        acciones()
+
         Log.v("ciclo_vida","Ejecutando metodo OnCreate")
+    }
+
+    private fun acciones() {
+
+        binding.botonSaludar.setOnClickListener {
+            var snackbar = Snackbar.make(it,
+                "Saludo: " + resources.getString(R.string.saludo_snackbar),
+                Snackbar.LENGTH_INDEFINITE)
+            snackbar.setAction("Ocultar"){
+                snackbar.dismiss()
+            }
+            snackbar.show()
+        }
+
     }
 
     override fun onStart() {
